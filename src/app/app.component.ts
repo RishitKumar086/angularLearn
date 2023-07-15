fa1d6c9b789c5713750a988070b1372ca11dd59c
Rishit <rishit1910kumar@gmail.com>
2023-07-15 21:20:29 +0530

    second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
+import { RoomsComponent } from './rooms/rooms.component';
 
 @Component({
   selector: 'hinv-root',
   templateUrl: './app.component.html',
-//  template:`<h1>Hello</h1>
-//  <p>angular</p>`,
-  styleUrls: ['./app.component.scss']
+  //  template:`<h1>Hello</h1>
+  //  <p>angular</p>`,
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hotelinventoryapp';
+  @ViewChild('name', { static: true }) name!: ElementRef;
+  ngOnInit(): void {
+    this.name.nativeElement.innertext = 'Hilton Hotel';
+  }
+  // @ViewChild('user', { read: ViewContainerRef }) vcr!: ViewContainerRef;
+  // ngOnInit(): void {
+  // }
+  // ngAfterViewInit(): void {
+  //   const componentRef = this.vcr.createComponent(RoomsComponent);
+  // }
 }
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,75 +1,106 @@
-import { Component, DoCheck, OnInit } from '@angular/core';
+import {
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnInit,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
 import { Room, RoomList } from './rooms';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'hinv-rooms',
   templateUrl: './rooms.component.html',
-  styleUrls: ['./rooms.component.scss']
+  styleUrls: ['./rooms.component.scss'],
 })
-export class RoomsComponent implements OnInit ,DoCheck{
-  hotelName='hilton hill';
-  noOfRooms=10;
-  hideRooms=false;
-  rooms : Room={
-    totalRooms:20,
-    availableRooms:10,
-    bookedRooms:5
-  }
-  title='Room List'
-  selectedRoom!:RoomList;
-  roomList:RoomList[]=[];
-  constructor(){}
-  toggle(){
-    this.hideRooms=!this.hideRooms;
-    this.title='Rooms List';
+export class RoomsComponent
+  implements OnInit, DoCheck, AfterViewInit, AfterViewChecked
+{
+  hotelName = 'hilton hill';
+  noOfRooms = 10;
+  hideRooms = false;
+  rooms: Room = {
+    totalRooms: 20,
+    availableRooms: 10,
+    bookedRooms: 5,
+  };
+  title = 'Room List';
+  selectedRoom!: RoomList;
+  roomList: RoomList[] = [];
+
+  @ViewChild(HeaderComponent) headerComponent!: HeaderComponent;
+
+  @ViewChildren(HeaderComponent)
+  headerChildrenComponent!: QueryList<HeaderComponent>;
+
+  constructor() {}
+  toggle() {
+    this.hideRooms = !this.hideRooms;
+    this.title = 'Rooms List';
   }
   ngDoCheck(): void {
     console.log('On changes is called');
   }
   ngOnInit(): void {
-    this.roomList=[ {
-      roomNumber:101,
-      roomType:'Deluxe Room',
-      amenities:'Air conditioner,Free Wifi,Tv,BathRoom,Kitchen',
-      price:500,
-      photos:'https://images.unsplash.com/photo-1586105251261-72a756497a11?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=958&q=80',
-      checkInTime:new Date('11-Nov-2021'),
-      checkOutTime:new Date('12-Nov-2021')
-    },
-    {
-      roomNumber:102,
-      roomType:'Deluxe Room',
-      amenities:'Air conditioner,Free Wifi,Tv,BathRoom,Kitchen',
-      price:1000,
-      photos:'https://images.unsplash.com/photo-1586105251261-72a756497a11?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=958&q=80',
-      checkInTime:new Date('11-Nov-2021'),
-      checkOutTime:new Date('12-Nov-2021')
-    },
-    {
-      roomNumber:103,
-      roomType:'Private Suite',
-      amenities:'Air conditioner,Free Wifi,Tv,BathRoom,Kitchen',
-      price:15000,
-      photos:'https://images.unsplash.com/photo-1586105251261-72a756497a11?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=958&q=80',
-      checkInTime:new Date('11-Nov-2021'),
-      checkOutTime:new Date('12-Nov-2021')
-    },]
+    // console.log(this.headerComponent);
+    this.roomList = [
+      {
+        roomNumber: 101,
+        roomType: 'Deluxe Room',
+        amenities: 'Air conditioner,Free Wifi,Tv,BathRoom,Kitchen',
+        price: 500,
+        photos:
+          'https://images.unsplash.com/photo-1586105251261-72a756497a11?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=958&q=80',
+        checkInTime: new Date('11-Nov-2021'),
+        checkOutTime: new Date('12-Nov-2021'),
+      },
+      {
+        roomNumber: 102,
+        roomType: 'Deluxe Room',
+        amenities: 'Air conditioner,Free Wifi,Tv,BathRoom,Kitchen',
+        price: 1000,
+        photos:
+          'https://images.unsplash.com/photo-1586105251261-72a756497a11?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=958&q=80',
+        checkInTime: new Date('11-Nov-2021'),
+        checkOutTime: new Date('12-Nov-2021'),
+      },
+      {
+        roomNumber: 103,
+        roomType: 'Private Suite',
+        amenities: 'Air conditioner,Free Wifi,Tv,BathRoom,Kitchen',
+        price: 15000,
+        photos:
+          'https://images.unsplash.com/photo-1586105251261-72a756497a11?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=958&q=80',
+        checkInTime: new Date('11-Nov-2021'),
+        checkOutTime: new Date('12-Nov-2021'),
+      },
+    ];
   }
-  selectRoom(room:RoomList){
-    this.selectedRoom=room;
+  ngAfterViewInit(): void {
+    // console.log(this.headerComponent);
+    this.headerComponent.title = 'Rooms View';
+    this.headerChildrenComponent.last.title = 'Last Title';
+  }
+  ngAfterViewChecked(): void {}
+  selectRoom(room: RoomList) {
+    this.selectedRoom = room;
   }
 
-  addRoom(){
-    const room:RoomList={
-      roomNumber:104,
-      roomType:'Private Suite',
-      amenities:'Air conditioner,Free Wifi,Tv,BathRoom,Kitchen',
-      price:15000,
-      photos:'https://images.unsplash.com/photo-1586105251261-72a756497a11?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=958&q=80',
-      checkInTime:new Date('11-Nov-2021'),
-      checkOutTime:new Date('12-Nov-2021')
+  addRoom() {
+    const room: RoomList = {
+      roomNumber: 104,
+      roomType: 'Private Suite',
+      amenities: 'Air conditioner,Free Wifi,Tv,BathRoom,Kitchen',
+      price: 15000,
+      photos:
+        'https://images.unsplash.com/photo-1586105251261-72a756497a11?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=958&q=80',
+      checkInTime: new Date('11-Nov-2021'),
+      checkOutTime: new Date('12-Nov-2021'),
     };
     // this.roomList.push(room);
-    this.roomList=[...this.roomList,room]
+    this.roomList = [...this.roomList, room];
   }
 }
